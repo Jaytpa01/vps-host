5c0ab142b91140066e4d8a6cf1d787f77cc86730
Jaytpa01 <jaytpa01@gmail.com>
2024-04-18 19:14:34 +1000

    finish script implementation

--- ufw/allow-cloudflare-ips-only.sh
+++ ufw/allow-cloudflare-ips-only.sh
@@ -4,11 +4,11 @@
 curl -s https://www.cloudflare.com/ips-v4/ | \
 while IFS="" read -r IP || [ -n "$IP" ];
 do
-    echo "Cloudflare IP $IP"
+    sudo ufw allow from $IP to any port 80,443 proto tcp
 done
 
 curl -s https://www.cloudflare.com/ips-v6/ | \
 while IFS="" read -r IPV6 || [ -n "$IPV6" ];
 do
-    echo "Cloudflare IPv6 $IPV6"
+    sudo ufw allow from $IPV6 to any port 80,443 proto tcp
 done
