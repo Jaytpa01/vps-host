118baec05da78b5258ac506f448a0a6468f3d65b
Jaytpa01 <jaytpa01@gmail.com>
2024-04-19 20:11:58 +1000

    fix syntax errors

--- ufw/allow-cloudflare-ips-only.sh
+++ ufw/allow-cloudflare-ips-only.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-OLD_RULES=$(sudo ufw status numbered \ 
-    # get current http(s) rules
-    | grep 80,443/tcp \ 
-    # get the numbers (i.e everything between brackets)
-    | awk -F "[][]" '{print 2}' \
-    # delete whitespace and sort reversed. We sort reversed so that the numbers down't change as we go through deleting the rules
+# get current http(s) rules
+# get the numbers (i.e everything between brackets)
+# delete whitespace and sort reversed. We sort reversed so that the numbers down't change as we go through deleting the rules
+OLD_RULES=$(sudo ufw status numbered \
+    | grep 80,443/tcp \
+    | awk -F "[][]" '{print $2}' \
     | tr --delete [:blank:] | sort -rn)
 
 for NUM in $OLD_RULES; do
@@ -20,3 +20,5 @@ done
 for IPV6 in $(curl -s https://www.cloudflare.com/ips-v6/); do
     sudo ufw allow from $IPV6 to any port 80,443 proto tcp
 done
+
+sudo ufw reload
