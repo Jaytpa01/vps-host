46a952860b444501e5c273527c4d7bdfff47e2b4
Jaytpa01 <jaytpa01@gmail.com>
2024-04-19 20:03:19 +1000

    update cloudflare ip script to delete already set rules
    this is done so that we can run this in a cron job

--- ufw/allow-cloudflare-ips-only.sh
+++ ufw/allow-cloudflare-ips-only.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-#!/bin/bash
-curl -s https://www.cloudflare.com/ips-v4/ | \
-while IFS="" read -r IP || [ -n "$IP" ];
+OLD_RULES=$(sudo ufw status numbered \ 
+    # get current http(s) rules
+    | grep 80,443/tcp \ 
+    # get the numbers (i.e everything between brackets)
+    | awk -F "[][]" '{print 2}' \
+    # delete whitespace and sort reversed. We sort reversed so that the numbers down't change as we go through deleting the rules
+    | tr --delete [:blank:] | sort -rn)
+
+for NUM in $OLD_RULES; do
+  yes | sudo ufw delete $NUM
+done
+
+for IP in $(curl -s https://www.cloudflare.com/ips-v4/); do
 do
     sudo ufw allow from $IP to any port 80,443 proto tcp
 done
 
-curl -s https://www.cloudflare.com/ips-v6/ | \
-while IFS="" read -r IPV6 || [ -n "$IPV6" ];
-do
+for IPV6 in $(curl -s https://www.cloudflare.com/ips-v6/); do
     sudo ufw allow from $IPV6 to any port 80,443 proto tcp
 done
